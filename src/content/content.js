e57c3ffa9f04d8133f54258af8292922a71b5eb5
Anton Florendo <antonflorendo7@gmail.com>
2026-03-19 23:36:34 -0700

    feat(content): add deduplication logic and   MutationObserver to track dynamically loaded   professors

--- src/content/content.js
+++ src/content/content.js
@@ -1,8 +1,23 @@
+console.log("SFU ProfessorView content script loaded");
+console.log(document.querySelectorAll('div.rightnclear[title="Instructor(s)"]'));
+
+const seen = new Set();
+
 const findProfessors = () => {
     const professorsArr = document.querySelectorAll('div.rightnclear[title="Instructor(s)"]');
 
-    professorsArr.forEach(prof => console.log(prof));
-
+    professorsArr.forEach(prof => {
+        if (!seen.has(prof)) {
+            seen.add(prof);
+            console.log(prof);
+        }
+    });
 }
 
 findProfessors();
+
+const observer = new MutationObserver(() => {
+    findProfessors();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
